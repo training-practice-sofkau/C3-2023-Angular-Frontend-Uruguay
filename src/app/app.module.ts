9b01f0b93d68a63c6b410cf89f258c4882f73cc6
Clyzer <clyzer.dev@gmail.com>
2023-02-06 14:17:37 -0300

    Reformed app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 import { LoginModule } from './login/login-module.module';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material/material.module';
 
 @NgModule({
@@ -20,7 +20,6 @@ import { MaterialModule } from './material/material.module';
     MaterialModule,
     SharedModule,
     LoginModule,
-
   ],
   providers: [],
   bootstrap: [AppComponent]
