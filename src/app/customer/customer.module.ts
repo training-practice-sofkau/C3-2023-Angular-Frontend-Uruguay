584f9c7708707e0bcf8d9b6f78debda57cdd8355
Franco Torres <franco.torres1705@gmail.com>
2023-02-06 12:13:19 -0300

    agrenando datos al ng module customer

--- src/app/customer/customer.module.ts
+++ src/app/customer/customer.module.ts
@@ -2,10 +2,13 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
 import { CustomerComponent } from './customer.component';
+import { GetCustomerComponent } from './get-customer/get-customer.component';
+import { UnsubscribeComponent } from './unsubscribe/unsubscribe.component';
+import { UpdateCustomerComponent } from './update-customer/update-customer.component';
 
 @NgModule({
   imports: [CommonModule],
-  declarations: [CustomerComponent],
+  declarations: [CustomerComponent,GetCustomerComponent,UnsubscribeComponent,UpdateCustomerComponent],
   exports: [CustomerComponent],
 })
 export class CustomerModule {}
