5dcf70ae42e34b815feaa9ad0aa8b26c6419431f
herweg <martindelgado2011@gmail.com>
2023-02-06 19:04:13 -0300

    Movements Module
    deposit

--- src/app/movements/movements.module.ts
+++ src/app/movements/movements.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MovementsComponent } from './movements.component';
+import { DepositComponent } from './deposit/deposit.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    MovementsComponent,
+    DepositComponent
+  ],
   imports: [
     CommonModule
   ]
