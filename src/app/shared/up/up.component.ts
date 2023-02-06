01614614c3d77a9500add47467573bf61baf36cb
Clyzer <clyzer.dev@gmail.com>
2023-02-06 18:11:10 -0300

    Change tailwind to primeng

--- src/app/shared/up/up.component.ts
+++ src/app/shared/up/up.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-up',
   templateUrl: './up.component.html',
-  styleUrls: ['./up.component.css']
+  styleUrls: ['./up.component.scss']
 })
 export class UpComponent {
 
