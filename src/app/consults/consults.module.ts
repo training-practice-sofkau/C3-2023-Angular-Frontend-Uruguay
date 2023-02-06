9c77af038d5650175c64ad6d228cd60bcdb47998
herweg <martindelgado2011@gmail.com>
2023-02-06 19:02:20 -0300

    Consults module
    account balance

--- src/app/consults/consults.module.ts
+++ src/app/consults/consults.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AccountComponent } from './account/account.component';
+import { BalanceComponent } from './balance/balance.component';
 
 
 
 @NgModule({
   declarations: [
-    AccountComponent
+    AccountComponent,
+    BalanceComponent
   ],
   imports: [
     CommonModule
