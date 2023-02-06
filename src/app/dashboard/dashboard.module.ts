5061a34db09d1eaedd580387fbdd1b0c8c26d1c0
Clyzer <clyzer.dev@gmail.com>
2023-02-06 17:12:49 -0300

    View component for dashboard

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ViewComponent } from './view/view.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ViewComponent
+  ],
   imports: [
     CommonModule
   ]
