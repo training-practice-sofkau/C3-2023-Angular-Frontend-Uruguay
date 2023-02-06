c7caa6ddfbc78d8328f7a4678d9a140016f1c924
federicogandaria <federicogandaria@gmail.com>
2023-02-06 14:48:24 -0300

    creacion de guard en login

--- src/app/account/account.module.ts
+++ src/app/account/account.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CreateAccountComponent } from './create-account/create-account.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    CreateAccountComponent
+  ],
   imports: [
     CommonModule
   ]
