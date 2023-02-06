aef97cc0f767c0bb13ddb7ead9d9c3a06b067ab8
Cristian Tironi <tironi741@gmail.com>
2023-02-06 15:12:03 -0300

    Component dashboard
    Agregamos el componente dashboard en el modulo account

--- src/app/account/account.module.ts
+++ src/app/account/account.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UserComponent } from './user/user.component';
 import { CreateAccountComponent } from './create-account/create-account.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 
 
 @NgModule({
   declarations: [
     UserComponent,
-    CreateAccountComponent
+    CreateAccountComponent,
+    DashboardComponent
   ],
   imports: [
     CommonModule
