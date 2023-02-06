926d74240012f5cea6b0fc057ab7700defcca0b5
Cristian Tironi <tironi741@gmail.com>
2023-02-06 15:10:09 -0300

    Component create-account
    Agregamos el componente create-account en el modulo account

--- src/app/account/account.module.ts
+++ src/app/account/account.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UserComponent } from './user/user.component';
+import { CreateAccountComponent } from './create-account/create-account.component';
 
 
 
 @NgModule({
   declarations: [
-    UserComponent
+    UserComponent,
+    CreateAccountComponent
   ],
   imports: [
     CommonModule
