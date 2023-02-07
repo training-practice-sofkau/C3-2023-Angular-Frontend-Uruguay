64224ff97705790e19419f2dcae8181459921843
matisouza <matias.souza@sofka.com.co>
2023-02-07 10:22:16 -0300

    week 3 day 2

--- src/app/prog-functional/ejercicio/ejercicio.component.ts
+++ src/app/prog-functional/ejercicio/ejercicio.component.ts
@@ -10,17 +10,24 @@ export class EjercicioComponent {
 
   /**
    * Aplicar 6 transformaciones de datos 3 con observables y 3 sin.
-   *
    * Se deben usar 2 operadores funcionales para cada transformacion.
    *
    * Se debe crear una funcion pura, a demas de la que dio el coach
    *
    * A partir de la funcion pura crear una composicion de funciones y
-   * una funcion de orden superior.
+   * una funcion de orden superior o un callback.
    *
    * Funcion de orden superior: Tomar una o más funciones como entrada y Devolver una función como salida
    * Comopsicion de funciones: Componer funciones se basa en combinar funciones simples para construir funciones más complicadas
    */
 
+
+
+  //funcion pura
+  transform(document: string) {
+    from(this.customers).pipe(
+      filter(n => n.document === document),
+    ).subscribe((data) => this.newData = data)
+  }
 
 }
