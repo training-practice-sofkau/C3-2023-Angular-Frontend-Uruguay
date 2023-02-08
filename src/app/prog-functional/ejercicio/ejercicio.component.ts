0b79bc0433965408cd015ab810a24a6e4c26e2e4
jhonatan239 <jhonatan239@hotmail.com>
2023-02-08 04:40:06 -0300

    Title: Correcion codigo
    Correccion sintaxis

--- src/app/prog-functional/ejercicio/ejercicio.component.ts
+++ src/app/prog-functional/ejercicio/ejercicio.component.ts
@@ -10,85 +10,87 @@ import { filter, from, map } from 'rxjs';
 })
 export class EjercicioComponent {
 
-public activeCustomer : {state: boolean}[] = []
-  public  selectedData: { name: string; document: string; }[] | undefined;
-  public  selectedData1: { name: string; document: string; }[] | undefined;
+  public activeCustomer: { state: boolean }[] = []
+  public selectedData: { name: string; document: string; }[] | undefined;
+  public selectedData1: { name: string; document: string; }[] | undefined;
   baseC = [...baseCustomers]
   public observadoN?: string;
   public observado1?: string;
   public customers: CustomerModel[] = baseCustomers;
-  obsState: string[] = [];
-  public  listCustomer: { name: string; }[] | undefined;
+  obsState: string[] = [];
+  public listCustomer: { name: string; }[] | undefined;
 
-  
+
 
   //observer
   searchByNameAndGetNameAndEmail(fullName: string) {
     from(this.baseC).pipe(
       filter((customer) => customer.fullName === fullName),
-     map((customer) => `El nombre del cliente es ${customer.fullName} y su correo es ${customer.email}`)
-   ).subscribe((name)=> this.observadoN = name);
-  return this.observadoN
-  }
+      map((customer) => `El nombre del cliente es ${customer.fullName} y su correo es ${customer.email}`)
+    ).subscribe((name) => this.observadoN = name);
+    return this.observadoN
+  }
 
-//observer
-searchByphone(phone: number) {
-  from(this.baseC).pipe(
-    filter((customer) => customer.phone === phone),
-   map((customer) => `El phone del cliente es ${customer.phone} y su nombre es ${customer.fullName}`)
- ).subscribe((name)=> this.observado1 = name);
-return this.observado1
-  }
-
-
-//observer
-filterCustomerByState() {
-  from(this.customers)
-    .pipe(
-      filter((customer) => customer.state === true),
-      map((customer) => `Cliente: ${customer.fullName.toUpperCase()} ID: ${customer.id}`))
-    .subscribe((obs1) => this.obsState.push(obs1));
-  return this.obsState;
-  }
+  //observer
+  searchByphone(phone: number) {
+    from(this.baseC).pipe(
+      filter((customer) => customer.phone === phone),
+      map((customer) => `El phone del cliente es ${customer.phone} y su nombre es ${customer.fullName}`)
+    ).subscribe((name) => this.observado1 = name);
+    return this.observado1
+  }
 
 
- //no observer
+  //observer
+  filterCustomerByState() {
+    from(this.customers)
+      .pipe(
+        filter((customer) => customer.state === true),
+        map((customer) => `Cliente: ${customer.fullName.toUpperCase()} ID: ${customer.id}`))
+      .subscribe((obs1) => this.obsState.push(obs1));
+    return this.obsState;
+  }
+
+
+  //no observer
   filterActiveCustomers(state: boolean) {
-     
+
     this.activeCustomer = baseCustomers.map(customer =>
-      ({state:customer.state,
-      name:customer.fullName, 
-    docType:customer.documentType.name})
-       ).filter(customer => customer.state === state) ;
-       
-}
+    ({
+      state: customer.state,
+      name: customer.fullName,
+      docType: customer.documentType.name
+    })
+    ).filter(customer => customer.state === state);
 
- //no observer
-//listar los usuarios con pasaporte y solo traer sus tipo de documento y nombre
-  listForDocType():void{
+  }
+
+  //no observer
+  //listar los usuarios con pasaporte y solo traer sus tipo de documento y nombre
+  listForDocType(): void {
     const filteredData = baseCustomers.filter(d => d.documentType.name == 'Pasaporte');
-    this.selectedData1 = filteredData.map(d => ({name: d.fullName, document: d.documentType.name}));  
-}
+    this.selectedData1 = filteredData.map(d => ({ name: d.fullName, document: d.documentType.name }));
+  }
 
- //no observer
-//Aplicar un cambo a un elemento de una registro *****
-modDocumentType():void{
-const documetMod = baseCustomers.map(d => ({...d, score: d.documentType.name = "Cedula"}));
- this.selectedData = documetMod.map(d => ({name: d.fullName, document: d.documentType.name}));  
+  //no observer
+  //Aplicar un cambo a un elemento de una registro *****
+  modDocumentType(): void {
+    const documetMod = baseCustomers.map(d => ({ ...d, score: d.documentType.name = "Cedula" }));
+    this.selectedData = documetMod.map(d => ({ name: d.fullName, document: d.documentType.name }));
 
-}
+  }
 
-// buscar por cedula 
-orderByName(numDoc:string):void{
-const filteredData = baseCustomers.filter(d => d.document == 'Pasaporte');
-const found = filteredData.find(n => n.document === numDoc);
+  // buscar por cedula 
+  orderByName(numDoc: string): void {
+    const filteredData = baseCustomers.filter(d => d.document == 'Pasaporte');
+    const found = filteredData.find(n => n.document === numDoc);
 
-}
+  }
 
-  
-//no se termino de implementar la idea
-getInfo(fn: (parametro: string[]) => void ){
-    const customersInfo = baseCustomers.map(usuario => 
+
+  //no se termino de implementar la idea
+  getInfo(fn: (parametro: string[]) => void) {
+    const customersInfo = baseCustomers.map(usuario =>
       `id de cuenta: ${usuario.id},
       id de tipo de documento: ${usuario.documentType.id}
       nombre del documento: ${usuario.documentType.name}
@@ -102,25 +104,25 @@ getInfo(fn: (parametro: string[]) => void ){
     fn(customersInfo);
   }
 
-//funcion Pura
-//getAllCust(): void{
-//this.listCustomer = baseCustomers.map(n => n.fullName);
-//return [listCustomer]
-//}
+  //funcion Pura
+  //getAllCust(): void{
+  //this.listCustomer = baseCustomers.map(n => n.fullName);
+  //return [listCustomer]
+  //}
 
 
-//FUNCION PURA
-getCustomerById = (id: string) => {
-  return baseCustomers.filter(baseCustomers => baseCustomers.id === id);
+  //FUNCION PURA
+  getCustomerById = (id: string) => {
+    return baseCustomers.filter(baseCustomers => baseCustomers.id === id);
 
-};
+  };
 
-getFullName = (customer: CustomerModel[]) => baseCustomers[0].fullName;
+  getFullName = (customer: CustomerModel[]) => baseCustomers[0].fullName;
 
- getCustomerFullNameById = (id: string, customers: CustomerModel[]) =>
-  this.getFullName(this.getCustomerById(id));
+  getCustomerFullNameById = (id: string, customers: CustomerModel[]) =>
+    this.getFullName(this.getCustomerById(id));
 
-//const logCustomerFullName = (id: string) => (customers: CustomerModel[]) =>
+  //const logCustomerFullName = (id: string) => (customers: CustomerModel[]) =>
   //console.log(getCustomerFullNameById(id, customers));
 
 }
