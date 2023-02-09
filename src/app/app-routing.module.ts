b713963bd80ff842d854353ba1173747e59b2b05
herweg <martindelgado2011@gmail.com>
2023-02-09 16:51:22 -0300

    Guard implemented

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { AccountComponent } from './consults/account/account.component';
+import { TransferComponent } from './movements/transfer/transfer.component';
 import { EjercicioComponent } from './prog-functional/ejercicio/ejercicio.component';
 import { HomeComponent } from './shared/home/home.component';
 import { NotfoundComponent } from './shared/notfound/notfound.component';
+import { GuardsGuard } from './guards.guard';
 
 const routes: Routes = [
 
@@ -12,9 +14,9 @@ const routes: Routes = [
 
   { path: 'app-home', component: HomeComponent },
   //nuevas rutas
-  { path: 'app-account', component: AccountComponent },
-  { path: 'app-ejercicio', component: EjercicioComponent },
-
+  { path: 'app-account', component: AccountComponent, canActivate: [GuardsGuard] },
+  { path: 'app-transfer', component: TransferComponent, canActivate: [GuardsGuard] },
+  { path: 'app-ejercicio', component: EjercicioComponent, canActivate: [GuardsGuard] },
   { path: '**', component: NotfoundComponent }
 ];
 
--- src/app/guards.guard.ts
+++ src/app/guards.guard.ts
@@ -1,16 +1,23 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
 import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GuardsGuard implements CanActivate {
+  
+  constructor(private router: Router) { }
 
   canActivate(
-    route: ActivatedRouteSnapshot,
+    next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+
+    if (localStorage.getItem('logged') === "true") {
+      return true;
+    }
+
+    this.router.navigate(['/app-home']);
+    return false;
   }
-  
 }
