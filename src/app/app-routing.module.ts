8f53fb7a71eea949dcbb296b5d7d1e6e4b708f51
brunof <dlqq@outlook.es>
2023-02-09 16:03:55 -0300

    Commit Route Customer Data

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './shared/home/home.component';
 import { EjercicioComponent } from './prog-functional/ejercicio/ejercicio/ejercicio.component';
+import { MyAccountsComponent } from './customer-account/my-accounts/my-accounts.component';
 
 const routes: Routes = [
 
   {path:  '', redirectTo: 'app-home', pathMatch:  'full' },
   {path: 'app-home', component: HomeComponent},
+  {path: 'app-my-accounts', component: MyAccountsComponent},
   {path: 'app-ejercicio', component: EjercicioComponent},
   {path: '**', component: HomeComponent}
 
