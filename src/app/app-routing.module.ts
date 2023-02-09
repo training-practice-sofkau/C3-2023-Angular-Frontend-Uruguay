00539f0a1e5ad8bb9b0baf9417833fbaebeec532
brunof <dlqq@outlook.es>
2023-02-09 16:27:39 -0300

    Commit my-account route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,13 +3,17 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './shared/home/home.component';
 import { EjercicioComponent } from './prog-functional/ejercicio/ejercicio/ejercicio.component';
 import { MyAccountsComponent } from './customer-account/my-accounts/my-accounts.component';
+import { UserProfileComponent } from './customer-account/user-profile/user-profile.component';
 
 const routes: Routes = [
 
   {path:  '', redirectTo: 'app-home', pathMatch:  'full' },
+
   {path: 'app-home', component: HomeComponent},
+  {path: 'app-user-profile', component: UserProfileComponent},
   {path: 'app-my-accounts', component: MyAccountsComponent},
   {path: 'app-ejercicio', component: EjercicioComponent},
+
   {path: '**', component: HomeComponent}
 
 
