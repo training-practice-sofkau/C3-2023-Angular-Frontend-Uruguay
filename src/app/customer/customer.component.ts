a6aef32f20987f3ef0509025dc16a56decb14fad
Franco Torres <franco.torres1705@gmail.com>
2023-02-09 13:56:31 -0300

    Cambios customer

--- src/app/customer/customer.component.ts
+++ src/app/customer/customer.component.ts
@@ -1,7 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild,   } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from '../login2/services/auth.service';
+import { AccountComponent } from '../account/account.component';
+import { customerInterface } from '../account/interface/customer-interface';
+import { AccountTypeinterface } from '../account/interface/Account-type-interface';
+import { AccountInterfaec } from '../account/interface/account-interface';
+import { UpdateCustomerComponent } from './update-customer/update-customer.component';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
+
   selector: 'app-customer',
   templateUrl: './customer.component.html',
 })
-export class CustomerComponent { }
+export class CustomerComponent implements OnInit  {
+  constructor(private modalService: NgbModal, private AccountComponent: AccountComponent) { }
+
+  public customer: customerInterface | undefined
+  public accountType: AccountTypeinterface | undefined
+  public persona: AccountInterfaec | undefined
+
+
+  openEditModal() {
+    this.modalService.open(UpdateCustomerComponent);
+  }
+
+
+
+   ngOnInit(): void {
+     this.AccountComponent.getDataAccount()
+     this.customer =  this.AccountComponent.getDatacustomer()
+     this.persona =  this.AccountComponent.getAccount()
+     this.accountType = this.AccountComponent.getDataAccountType();
+
+
+
+  }
+  editMode = false;
+
+
+
+
+}
--- src/app/customer/customer.module.ts
+++ src/app/customer/customer.module.ts
@@ -1,13 +1,16 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { CustomerComponent } from './customer.component';
 import { UnsubscribeComponent } from './unsubscribe/unsubscribe.component';
 import { UpdateCustomerComponent } from './update-customer/update-customer.component';
+import { AccountComponent } from '../account/account.component';
 
 @NgModule({
-  imports: [CommonModule],
-  declarations: [CustomerComponent,UnsubscribeComponent,UpdateCustomerComponent],
+  imports: [CommonModule,FormsModule,ReactiveFormsModule],
+  declarations: [CustomerComponent,UnsubscribeComponent,UpdateCustomerComponent,],
   exports: [CustomerComponent],
+  providers:[AccountComponent]
 })
 export class CustomerModule {}
