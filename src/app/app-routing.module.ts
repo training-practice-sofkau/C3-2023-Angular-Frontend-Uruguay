8319659e576a6cd7512ce7cbcb2d8ce69860c862
Cristian Tironi <tironi741@gmail.com>
2023-02-09 19:04:46 -0300

    Estructura
    Actualizamos la estructura de carpetas del proyecto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './shared/home/home.component';
 import { EjercicioComponent } from './prog-functional/ejercicio/ejercicio.component';
-import { PageNotFoundComponent } from './shared/page-not-found/page-not-found.component';
-import { TransferComponent } from './transaction/transfer/transfer.component';
-import { SearchComponent } from './components/search/search.component';
-import { DepositComponent } from './transaction/deposit/deposit.component';
-import { GuardGuard } from './shared/guards/guard.guard';
+import { SearchComponent } from './dashboard/components/search/search.component';
+import { GuardGuard } from './modules/shared/guards/guard.guard';
+import { HomeComponent } from './modules/shared/home/home.component';
+import { PageNotFoundComponent } from './modules/shared/page-not-found/page-not-found.component';
+import { DepositComponent } from './dashboard/transaction/deposit/deposit.component';
+import { TransferComponent } from './dashboard/transaction/transfer/transfer.component';
+
 
 const routes: Routes = [
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SharedModule } from './shared/shared.module';
-import { LoginModule } from './login/login-module.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MaterialModule } from './material/material.module';
 import { EjercicioComponent } from './prog-functional/ejercicio/ejercicio.component';
-import { HomeComponent } from './shared/home/home.component';
+import { LoginModule } from './modules/login/login-module.module';
+import { MaterialModule } from './modules/material/material.module';
+import { SharedModule } from './modules/shared/shared.module';
+
 
 @NgModule({
   declarations: [
