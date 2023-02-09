59ddc8aa4176032c69ac98b40a0b561672cac750
federicogandaria <federicogandaria@gmail.com>
2023-02-09 15:07:33 -0300

    se agrega el crear tipo de cuenta a la ruta

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CreateAccountComponent } from './account/create-account-type/create-account.component';
 import { SinginComponent } from './login/singin/singin.component';
 import { SingupComponent } from './login/singup/singup.component';
 
+
 const routes: Routes = [
   { path: 'singup', component: SingupComponent },
-  {path: "singin", component: SinginComponent}
+  {path: "singin", component: SinginComponent},
+  {path: "createaccount", component: CreateAccountComponent}
 ];
 
 @NgModule({
