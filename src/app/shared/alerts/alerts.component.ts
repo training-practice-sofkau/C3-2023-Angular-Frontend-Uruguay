bb073c9c5a52afa299851328e9dab8bc7c719e9d
Franco Torres <franco.torres1705@gmail.com>
2023-02-09 16:40:02 -0300

    Swettalert

---
+++ src/app/shared/alerts/alerts.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import Swal from 'sweetalert2';
+import { Alerts } from './alerts.component.stories';
+@Component({
+  selector: 'app-alerts',
+  templateUrl: './alerts.component.html',
+})
+export class AlertsComponent {
+
+showModal(){
+  Swal.fire({
+    title: 'Estas seguro?',
+    text: "Esto nos e puede revertir!",
+    icon: 'warning',
+    showCancelButton: true,
+    confirmButtonColor: '#3085d6',
+    cancelButtonColor: '#d33',
+    confirmButtonText: 'Yes, delete it!'
+  }).then((result) => {
+    if (result.isConfirmed) {
+      Swal.fire(
+        'Deleted!',
+        'Your file has been deleted.',
+        'success'
+      )
+    }
+  })
+}
+
+
+}
--- src/app/transfer/transfer.component.ts
+++ src/app/transfer/transfer.component.ts
@@ -1,7 +1,18 @@
 import { Component } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import {NewTransferComponent} from './new-transfer/new-transfer.component'
 
 @Component({
   selector: 'app-transfer',
   templateUrl: './transfer.component.html',
 })
-export class TransferComponent { }
+export class TransferComponent {
+
+  constructor( private modalService: NgbModal ) {}
+
+  openEditModal() {
+    this.modalService.open(NewTransferComponent);
+  }
+
+
+}
