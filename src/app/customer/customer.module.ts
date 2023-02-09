765890b7a1b06a3bf99bbca02203f708acc372ae
Franco Torres <franco.torres1705@gmail.com>
2023-02-09 16:00:22 -0300

    cambios esteticos

--- src/app/customer/customer.module.ts
+++ src/app/customer/customer.module.ts
@@ -6,9 +6,10 @@ import { CustomerComponent } from './customer.component';
 import { UnsubscribeComponent } from './unsubscribe/unsubscribe.component';
 import { UpdateCustomerComponent } from './update-customer/update-customer.component';
 import { AccountComponent } from '../account/account.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
-  imports: [CommonModule,FormsModule,ReactiveFormsModule],
+  imports: [CommonModule,FormsModule,ReactiveFormsModule, RouterModule],
   declarations: [CustomerComponent,UnsubscribeComponent,UpdateCustomerComponent,],
   exports: [CustomerComponent],
   providers:[AccountComponent]
