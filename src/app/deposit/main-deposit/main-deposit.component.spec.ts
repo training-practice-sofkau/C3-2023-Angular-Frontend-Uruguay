6e509299c80c5f4bc88ebe79360f57702418fb1d
Rodrigo Caraballo <rodrigomcaraballo8@gmail.com>
2023-02-09 13:47:56 -0300

    Main Deposit
    Deposit main page, using components

---
+++ src/app/deposit/main-deposit/main-deposit.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { MainDepositComponent } from './main-deposit.component';
+
+describe('MainDepositComponent', () => {
+  let component: MainDepositComponent;
+  let fixture: ComponentFixture<MainDepositComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ MainDepositComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(MainDepositComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/deposit/main-deposit/main-deposit.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-main-deposit',
+  templateUrl: './main-deposit.component.html',
+  styleUrls: ['./main-deposit.component.scss']
+})
+export class MainDepositComponent {
+
+}
