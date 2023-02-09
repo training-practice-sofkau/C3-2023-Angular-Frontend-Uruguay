453421bebc0595aab18d1731d209114144c7bd22
Cristian Tironi <tironi741@gmail.com>
2023-02-09 16:40:17 -0300

    Guard
    Creamos e implementamos el guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,4 +1,4 @@
-import { NgModule, Component } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './shared/home/home.component';
 import { EjercicioComponent } from './prog-functional/ejercicio/ejercicio.component';
@@ -6,20 +6,21 @@ import { PageNotFoundComponent } from './shared/page-not-found/page-not-found.co
 import { TransferComponent } from './transaction/transfer/transfer.component';
 import { SearchComponent } from './components/search/search.component';
 import { DepositComponent } from './transaction/deposit/deposit.component';
+import { GuardGuard } from './shared/guards/guard.guard';
 
 const routes: Routes = [
 
-  {
+{
     path: '',
     redirectTo: 'app-home',
     pathMatch: 'full',
 },
 
 { path: 'home', component: HomeComponent },
-{ path: 'ejercicio', component: EjercicioComponent },
-{ path: 'transfer', component: TransferComponent },
-{ path: 'search', component: SearchComponent },
-{ path: 'deposit', component: DepositComponent },
+{ path: 'ejercicio', component: EjercicioComponent, canActivate: [GuardGuard] },
+{ path: 'transfer', component: TransferComponent, canActivate: [GuardGuard] },
+{ path: 'search', component: SearchComponent, canActivate: [GuardGuard] },
+{ path: 'deposit', component: DepositComponent, canActivate: [GuardGuard] },
 
 {
   path: '**',
