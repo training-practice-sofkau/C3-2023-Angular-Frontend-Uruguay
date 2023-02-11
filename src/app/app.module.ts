9ec1cf05feaa088f2c31cbd6097fe29f2f431091
brunof <dlqq@outlook.es>
2023-02-11 19:48:09 -0300

    Commit Lazy Loading Completos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,8 +6,8 @@ import { SharedModule } from './shared/shared.module';
 import { LoginModule } from './login/login-module.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material/material.module';
-import { CustomerAccountModule } from './customer-account/customer-account.module';
-import { TransactionsModule } from './transactions/transactions.module';
+import { CustomerAccountModule } from './customer-account/customer-account.module'; //se puede retirar?
+import { TransactionsModule } from './transactions/transactions.module'; //se puede retirar?
 import { HomeComponent } from './shared/home/home.component';
 import { EjercicioComponent } from './prog-functional/ejercicio/ejercicio/ejercicio.component';
 import { HttpClientModule } from '@angular/common/http';
