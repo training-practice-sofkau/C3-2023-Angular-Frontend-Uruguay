8d4d15f32bc350e736692a6f9cf7d03496ec8f02
Rodrigo Caraballo <rodrigomcaraballo8@gmail.com>
2023-02-12 20:54:10 -0300

    Customer Folder
    Resume: Can show the info of customer and remove service folder

--- src/app/customer/index.ts
+++ src/app/customer/index.ts
@@ -2,6 +2,4 @@ export * from './customer-change-password';
 export * from './customer-info';
 export * from './customer-routing.module';
 export * from './customer.module';
-export * from './guards';
 export * from './modify-customer';
-export * from './services';
