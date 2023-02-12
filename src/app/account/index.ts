1eb11b0f15095cb6bbbee32862cd4ec1c6d00b17
Rodrigo Caraballo <rodrigomcaraballo8@gmail.com>
2023-02-12 20:53:05 -0300

    Account Folder
    Resume: Implement function to show account info, remove service and modify some components

--- src/app/account/index.ts
+++ src/app/account/index.ts
@@ -1,7 +1,5 @@
 export * from './account-btns';
 export * from './account-dashboard';
 export * from './account-info';
-export * from './account-movements';
 export * from './account-routing.module';
 export * from './account.module';
-export * from './services';
