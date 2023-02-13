84802bb534571a376631cdbc2d4e9d73d05c8463
Clyzer <clyzer.dev@gmail.com>
2023-02-13 16:34:21 -0300

    Lazy changes

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -8,7 +8,23 @@ import { LastMovementsCustomerComponent } from './view/lists/last-movements-cust
 import { CurrencyComponent } from './view/lists/currency/currency.component';
 import { MonthlyExpensesComponent } from './view/lists/monthly-expenses/monthly-expenses.component';
 import { ProfileComponent } from './customer/profile/profile.component';
+import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from '../login/guards/auth.guard';
+import { SigninComponent } from '../login/sign-in/sign-in.component';
+import { SignupComponent } from '../login/sign-up/sign-up.component';
+import { AccountsTableComponent } from '../shared/accounts-table/accounts-table.component';
+import { DepositComponent } from './account/deposit/deposit.component';
 
+const routes: Routes = [
+  { path: 'sign-up', title: 'Sign-up', loadChildren: () => import('../login/login-module.module').then(m => m.LoginModule),
+  canActivate: [ AuthGuard ], component: SignupComponent },
+  { path: 'sign-in', title: 'Sign-in', loadChildren: () => import('../login/login-module.module').then(m => m.LoginModule),
+  canActivate: [ AuthGuard ], component: SigninComponent },
+  { path: 'dashboard', title: 'Dashboard', loadChildren: () => import('../dashboard/dashboard.module').then(m => m.DashboardModule),
+  canActivate: [ AuthGuard ], component: ViewComponent },
+  { path: 'deposits', title: 'Deposits', loadChildren: () => import('../dashboard/account/account.module').then(m => m.AccountModule),
+  canActivate: [ AuthGuard ], component: DepositComponent }
+]
 @NgModule({
     declarations: [
         ViewComponent,
@@ -25,7 +41,8 @@ import { ProfileComponent } from './customer/profile/profile.component';
     imports: [
         CommonModule,
         AccountModule,
-        SharedModule
+        SharedModule,
+        RouterModule.forChild(routes)
     ]
 })
 export class DashboardModule { }
