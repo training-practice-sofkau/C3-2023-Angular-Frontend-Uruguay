563e26764c7d6b0b686c759cfbad855e9ff75ee4
Rodrigo Caraballo <rodrigomcaraballo8@gmail.com>
2023-02-13 22:14:53 -0300

    Customer
    Module change

--- src/app/customer/customer.module.ts
+++ src/app/customer/customer.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CustomerChangePasswordComponent, CustomerInfoComponent, ModifyCustomerComponent } from './';
 import { FormsModule } from '@angular/forms';
+import { CustomerRoutingModule } from './customer-routing.module';
 
 
 
@@ -13,7 +14,7 @@ import { FormsModule } from '@angular/forms';
   ],
   imports: [
     CommonModule,
-    FormsModule
+    FormsModule,
   ],
   exports: [CustomerChangePasswordComponent,
     CustomerInfoComponent,
