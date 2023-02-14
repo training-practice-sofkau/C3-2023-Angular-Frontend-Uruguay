f4a58cc1798317eb4c1576caa4fb0cdb52238f8d
Edinson Acosta Gancharov <acostaedinson11@gmail.com>
2023-02-13 22:18:37 -0300

    Profile page done
    + customer profile information page updated and fixed
    + some code clean up and update

--- src/app/components/desktop/desktop-routing.module.ts
+++ src/app/components/desktop/desktop-routing.module.ts
@@ -1,12 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DesktopComponent } from './desktop.component';
+import { ProfileComponent } from './profile/profile.component';
+import { DetailsComponent } from './details/details.component';
 
 
 const routes: Routes = [
 
 
   { path: '', component: DesktopComponent, pathMatch: 'full' },
+
+
+  /* children: [
+  //{ path: 'desktop',  pathMatch: 'full', },
+  { path: 'desktop', component: DetailsComponent },
+  { path: 'desktop/profile', component: ProfileComponent },
+  ]}, */
+
+
+
+
 
 
 ]
--- src/app/components/desktop/desktop.component.ts
+++ src/app/components/desktop/desktop.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { CustomerModel } from '../../interfaces/customer.interface';
 import { CustomerService } from '../../services/customer.service';
+
 
 @Component({
   selector: 'app-desktop',
@@ -19,21 +19,27 @@ export class DesktopComponent implements OnInit {
   currentBalance: number = 0;
   customerName: string = '';
 
+
+
   constructor(
     private customerService: CustomerService,
+
   ) { }
 
   ngOnInit(): void {
     this.currentAccount = loadCustomerID();
     this.customerService.getCustomerData(this.currentAccount);
+    this.customerService.getCustomerAccounts(loadCustomerID()) ;
+
     this.currentBalance = getCustomerBalance();
-    this.customerName = ' Dear Customer ' //getCustomerName();
+    this.customerName = getCustomerName();
 
     this.showMain();
   }
 
 
   showMain(){
+
     this.showMainPage = true;
     this.showProfilePage = false;
   }
@@ -62,13 +68,15 @@ return 0;
 
 function getCustomerName(): string {
 
-  let name = localStorage.getItem("customer");
+  let name: string = "";
+  let customerData = localStorage.getItem("customer");
 
-
-
-  if(name === null) name = "";
-
+  if( customerData != null) {
+    let jsonObj = JSON.parse(customerData);
+    name = jsonObj.fullname;
+  }
   return name;
 
 }
+
 
